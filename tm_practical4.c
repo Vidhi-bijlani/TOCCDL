d0cbe7b86ba43f6447a32c9c6e73bb8abfb5e139
Vidhi Bijlani <dharmendrabijlani@gmail.com>
2026-08-19 12:26:51 +0530

    Create tm_practical4.c

---
+++ tm_practical4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+ char tape[100];
+ int left, right;
+ int palindrome = 1;
+ printf ("Enter a string: ");
+ scanf("%99s", tape);
+ left = 0;
+ right = strlen(tape) - 1;
+ while(left < right)
+ {
+ if(tape[left] != tape[right])
+ {
+ palindrome = 0;
+ break;
+ }
+ left++;
+ right--;
+ }
+ if(palindrome)
+ printf("String Accepted (Palindrome)\n");
+ else
+ printf ("String Rejected (Not a Palindrome) \n");
+ return 0;
+ }
